26224758b7ef6a31342c34b058675eaec7ef82c4
Ruslan Shayahmetov <raketaboy2000@yandex.ru>
2020-01-18 21:15:04 +0300

    изменение лого

--- config/adminlte.php
+++ config/adminlte.php
@@ -31,7 +31,7 @@ return [
     */
 
     'logo' => '<b>Trade</b>MATE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => '/vendor/adminlte/dist/img/logo.jpg',
     'logo_img_class' => 'brand-image-xl',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
